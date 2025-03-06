c45139e8eb3820290b11f5bcbd180968e9525a17
Brian <briansubekti11@gmail.com>
2025-03-06 13:37:07 +0700

    [REFACTOR] Apply OrderStatus enum check to Order model.

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -1,8 +1,13 @@
+
 package id.ac.ui.cs.advprog.eshop.model;
+
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+
+import java.util.List;
+import java.util.Arrays;
+
 import lombok.Builder;
 import lombok.Getter;
-import java.util.Arrays;
-import java.util.List;
 
 @Builder
 @Getter
@@ -12,18 +17,17 @@ public class Order {
     Long orderTime;
     String author;
 
-
     String status;
 
     public Order(String id, List<Product> products, Long orderTime, String author) {
         this.id = id;
         this.orderTime = orderTime;
         this.author = author;
-        this.status = "WAITING_PAYMENT";
+        this.status = OrderStatus.WAITING_PAYMENT.getValue();
 
         if (products.isEmpty()) {
             throw new IllegalArgumentException();
-        } else{
+        } else {
             this.products = products;
         }
     }
@@ -31,24 +35,16 @@ public class Order {
     public Order(String id, List<Product> products, Long orderTime, String author, String status) {
         this(id, products, orderTime, author);
 
-        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
-        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
-            throw new IllegalArgumentException();
-        } else {
-            this.status = status;
-        }
+        this.setStatus(status);
+
     }
 
     public void setStatus(String status) {
-        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
-
-        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
-            throw new IllegalArgumentException();
-        } else {
+        if (OrderStatus.contains(status)) {
             this.status = status;
         }
+        else {
+            throw new IllegalArgumentException();
+        }
     }
-
-
-
 }
